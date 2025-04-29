b44f0106520ee4bf71cbb98962b5eef310af35b6
Danton Lucas <danyonlucasrr@gmail.com>
2025-04-29 20:09:12 -0300

    Create: criaçao da primeira View, e conhecendo a funcao view() e compact() tambem conhecendo a existencia do Request

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -1,21 +1,25 @@
 <?php
-
+// o controller recebe a requisiçao de forma que ele possa tratar a requisiçao e devolvendo  a resposta em formatos variados 
 namespace App\Http\Controllers;
 
-class SeriesController
+use Illuminate\Http\Request;
+//serve para ter acesso a funcao $request, para podermos pegar detalhes da requisiçao feita à este controller
+class SeriesController extends Controller
+
 {
-    public function listarSeries()
+    public function index()
     {
+        //nunca usar echo em um controller
         $series = [
             'Punisher',
             'Lost',
-            'Greys Anatomy'
+            'Greys Anatomy',
+            'The Walking Dead'
         ];
-        $html = '<ul>';
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= '</ul>';
-        echo $html;
+        return view('listar-series',compact('series'));
+        //estou retornando uma view() essa funcao espera um parametro 'nome-Da-View' e o segundo parametro opcional que são dados que a view pode usar 
+        // a funcao compact()  pega o nome de variaveis e cria um array associando esses nomes aos valores das variaveis
+        //neste caso estou chamando a view listar-series e passando os dados (arrays) series 
+        //o laravel analisa a melhor forma de exibir essa resposta, seja string ou Json etc... 
     }
 }
